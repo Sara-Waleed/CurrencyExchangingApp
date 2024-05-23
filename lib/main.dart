b62f7ec80b4bd0d29e78dce623845fb214b827af
Sara-Waleed <sarawaleedmohamed2@gmail.com>
2024-05-23 20:59:13 +0300

    1st

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,142 @@
-import 'package:currenyexchanging/screens/ExchangePage.dart';
-import 'package:currenyexchanging/screens/HomePage.dart';
-import 'package:currenyexchanging/widgets/AppBarTitle.dart';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
+
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  // This widget is the root of your application.
-  int currentIndex=0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar:AppBar(
-          title: MainAppBar(title: "Currency"),
-        ) ,
-        body:  Center(
-          child: currentIndex == 0
-              ?  HomePage()
-              : currentIndex == 1
-              ? ExchangePage(): ExchangePage()
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(
+      themeMode: ThemeMode.dark,
+      home: App(),
+    );
+  }
+}
 
-                icon: Icon(Icons.home),
-                label: "Home"),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.currency_exchange_outlined),
-                label: "Exchange"),
-          ],
-          currentIndex: currentIndex,
-          onTap: (int index){
-            setState(() {
-              currentIndex=index;
-            });
-          },
+
+
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  TextEditingController controllerr =TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+              SizedBox(height: 80,),
+              Text("Convert anything ",style: TextStyle(
+                color: Colors.black,
+                fontSize: 50,
+                fontWeight: FontWeight.bold
+              ),),
+
+              SizedBox(height: 60,),
+
+              TextFormField(
+                onChanged: (value) {
+                  controllerr.text = value; // Update the controller's text
+                },
+                onSaved: (newValue) {
+                  controllerr.text = newValue ?? ''; // Ensure newValue is not null
+                },
+                controller: controllerr,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  hintText: "input valid value",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 40,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    width: 120,
+                 height: 50,
+                    child:MyDropdownButton(),
+                  ),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade300,
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: Icon(Icons.currency_exchange_outlined),
+
+                  ), Container(
+                     width: 120,
+                     height: 50,
+                     child:MyDropdownButton(),
+
+                  ),
+                ],
+              ),
+              SizedBox(height: 40,),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade300,
+                  borderRadius: BorderRadiusDirectional.circular(20),
+                ),
+                width: double.infinity,
+                height: 200,
+
+
+                child: Center(child: Text("Result")),),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
 
+class MyDropdownButton extends StatefulWidget {
+  @override
+  _MyDropdownButtonState createState() => _MyDropdownButtonState();
+}
+
+class _MyDropdownButtonState extends State<MyDropdownButton> {
+  String? _selectedValue;
+  final List<String> _dropdownItems = ['Option 1', 'Option 2', 'Option 3'];
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: _selectedValue,
+      hint: Text('Select'),
+      items: _dropdownItems.map((String item) {
+        return DropdownMenuItem<String>(
+          value: item,
+          child: Text(item),
+        );
+      }).toList(),
+      onChanged: (String? newValue) {
+        setState(() {
+          _selectedValue = newValue;
+        });
+      },
     );
   }
 }
